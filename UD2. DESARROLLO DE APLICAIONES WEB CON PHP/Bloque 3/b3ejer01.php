3badeeabad1efaa5a3449454b6c09a827747e1bf
Elshin Vadim <elshinvadim@gmail.com>
2025-10-08 10:36:55 +0200

    Finished Block5, now to polish

--- UD2. DESARROLLO DE APLICAIONES WEB CON PHP/Bloque 3/b3ejer01.php
+++ UD2. DESARROLLO DE APLICAIONES WEB CON PHP/Bloque 3/b3ejer01.php
@@ -2,11 +2,7 @@
 if (!isset($_GET['num1']) || !isset($_GET['num2'])) {
     echo "<h2>Uno o más valores no están establecidos. Especifique el valor de num1 y num2</h2>";
 } elseif (ctype_alpha($_GET['num1']) || ctype_alpha($_GET['num2'])) {
-<<<<<<< HEAD
-    echo "<h2>Los valores deben ser números</h2>";
-=======
     echo "<h2 style='text-align: center;' >Los valores deben ser numericos</h2>";
->>>>>>> dd5d9fc7a1f039d2cb388c0796c7f220d9f010fa
 } else {
     $num1 = $_GET['num1'];
     $num2 = $_GET['num2'];
--- UD2. DESARROLLO DE APLICAIONES WEB CON PHP/Bloque 3/b3ejer04.php
+++ UD2. DESARROLLO DE APLICAIONES WEB CON PHP/Bloque 3/b3ejer04.php
@@ -1,23 +1,14 @@
 <?php
 if (!isset($_GET['password']) || !isset($_GET['encriptar'])) {
-<<<<<<< HEAD
-    echo "<h2>Uno o más valores no están establecidos. Especifique el valor de password y encriptar</h2>";
-} elseif ($_GET['encriptar'] == "md5" || $_GET['encriptar'] == "sha256" || $_GET['encriptar'] == "crc32") {
-=======
     echo "<h2>No se pudo encontrar unos valores</h2><br>";
 }
 elseif($_GET['encriptar'] == "md5" || $_GET['encriptar'] == "sha256" || $_GET['encriptar'] == "crc32") {
->>>>>>> dd5d9fc7a1f039d2cb388c0796c7f220d9f010fa
     // $password = $_GET['password'];
     // $encriptar = $_GET['encriptar'];
     echo "Tú contraseña: " . $_GET['password'] . "<br>";
     echo "El algoritmo de encryptación: " . $_GET['encriptar'] . "<br>";
     echo "La contraseña encryptada: " . hash($_GET['encriptar'], $_GET['password']);
-<<<<<<< HEAD
-} else {
-=======
 }else{
->>>>>>> dd5d9fc7a1f039d2cb388c0796c7f220d9f010fa
     echo "El algoritmo no reconocido<br>";
 }
 ?>
