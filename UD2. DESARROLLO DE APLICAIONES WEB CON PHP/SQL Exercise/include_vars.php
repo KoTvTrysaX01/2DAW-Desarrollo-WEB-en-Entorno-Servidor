8aa6b3c7d9ee3d9aa9fd20af88bf25a20b5dbaf4
Elshin Vadim <elshinvadim@gmail.com>
2025-10-24 20:25:22 +0200

    Fixed exercise

--- UD2. DESARROLLO DE APLICAIONES WEB CON PHP/SQL Exercise/include_vars.php
+++ UD2. DESARROLLO DE APLICAIONES WEB CON PHP/SQL Exercise/include_vars.php
@@ -4,21 +4,10 @@
 $hostSql = "127.0.0.1";
 $userSql = "root";
 $passSql = "";
-$basedatosSql = "daw_tienda";
+$basedatosSql = "tienda";
 
 // Variables de instrucciones de MySQL
 $errorSql = "";
 $continuaSql = true;
-
-
-// Cargar variables de conexión a MySQL si está en riconet.es
-if ( 
-		($_SERVER['HTTP_HOST']=="riconet.es") && 
-		(file_exists($_SERVER['DOCUMENT_ROOT']."/fp/DWES/include_ionos.php")) 
-	) {
-	require_once $_SERVER['DOCUMENT_ROOT']."/fp/DWES/include_ionos.php";
-}
-
-
 
 ?>
