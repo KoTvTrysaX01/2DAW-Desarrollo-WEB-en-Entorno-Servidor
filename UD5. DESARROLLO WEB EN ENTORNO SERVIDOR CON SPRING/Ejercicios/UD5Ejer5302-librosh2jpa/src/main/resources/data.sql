5fdd928bd1cfc7b5ee9be0377dbf51b584177d9f
Elshin Vadim <elshinvadim@gmail.com>
2026-01-14 13:06:46 +0100

    Finished 5302

---
+++ UD5. DESARROLLO WEB EN ENTORNO SERVIDOR CON SPRING/Ejercicios/UD5Ejer5302-librosh2jpa/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO libros ( id, titulo, autor ) VALUES 
+  (1, 'Macbeth', 'William Shakespeare'),
+  (2, 'La Celestina (Tragicomedia de Calisto y Melibea)', 'Fernando de Rojas'),
+  (3, 'El Lazarillo de Tormes', 'Anónimo'),
+  (4, '20.000 Leguas de Viaje Submarino', 'Julio Verne'),
+  (5, 'Alicia en el País de las Maravillas', 'Lewis Carrol'),
+  (6, 'Cien Años de Soledad', 'Gabriel García Márquez'),
+  (7, 'La tempestad', 'William Shakespeare');
